a9041343bef5116486ccdcab5320b304616eeb68
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-02 11:37:03 +0100

    ntn

--- PASSPORT/app.js
+++ PASSPORT/app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const startDB = require("./config/connectDB");
-const gigRoutes = require("./routes/gigRoutes");
 const app = express();
 
 app.use(express.json());
@@ -12,9 +11,7 @@ app.get("/", (req, res) => {
   res.send("INDEX");
 });
 
-app.use("/api/v1/gig", gigRoutes);
-
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT | 3000;
 app.listen(PORT, () => {
   startDB();
   console.log(`Server is listening on port: ${PORT}`);
